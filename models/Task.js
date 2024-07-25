ecbc1d9e17d7456a4e97fe73e4e50e4731c39319
Sybil <sybilcelestra@gmail.com>
2024-07-24 18:09:43 -0700

    [feature][Connections]database
    features connections to database

--- models/Task.js
+++ models/Task.js
@@ -1,34 +1,11 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const TaskSchema = Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  description: {
-    type: String,
-    trim: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  priority: {
-    type: String,
-    enum: ['Low', 'Medium', 'High'],
-  },
-  status: {
-    type: String,
-    enum: ['To Do', 'In Progress', 'Done'],
-    default: 'To Do'
-  },
-  dueDate: {
-    type: Date
-  }
-}, {
-  timestamps: true
+const taskSchema = new mongoose.Schema({
+    name: String,
+    assignee: String,
+    dueDate: Date,
+    priority: { type: String, enum: ['High', 'Low'] },
+    status: { type: String, enum: ['To-do', 'Ongoing', 'Complete'] }
 });
 
-module.exports = mongoose.model('Task', TaskSchema);
+module.exports = mongoose.model('Task', taskSchema);
